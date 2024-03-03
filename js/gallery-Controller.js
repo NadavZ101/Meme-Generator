90584146704f4cb380735ccbb0988bf669e45195
NadavZ101 <nadavzivyon@gmail.com>
2024-03-03 20:21:29 +0200

    Added search a meme

--- js/gallery-Controller.js
+++ js/gallery-Controller.js
@@ -1,8 +1,24 @@
 'use strict'
 
-function renderGallery() {
+var gIsFiltered = false
+var gFilteredImgs = []
 
-    const imgs = loadGallery()
+// function renderSearchList() {
+//     const keywords = getKeywords()
+//     const strMemeListHTMLs = keywords.map(keyword => {
+//         return ` <option value=${keyword}></option>`
+//     })
+//     const elMemeTypes = document.querySelector('.dropdown')
+//     elMemeTypes.innerHTML = strMemeListHTMLs.join('')
+// }
+
+function renderGallery() {
+    if (gFilteredImgs.length === 0) {
+        var imgs = loadGallery()
+    } else {
+        imgs = gFilteredImgs
+    }
+
     const strImgsHTMLs = imgs.map(img => {
         return `<img id=${img.id} src="${img.url}" keywords="${img.keywords}"        onclick="onImgSelect(${img.id}, this)"></img>`
     })
@@ -16,5 +32,11 @@ function onImgSelect(imgId) {
     renderMeme()
     hideGallery()
     showEditor()
+}
+
+function onSearchMeme(keyword) {
+    console.log(keyword)
+    gFilteredImgs = setFilterBy(keyword)
+    renderGallery()
 }
 
--- js/service/meme-Service.js
+++ js/service/meme-Service.js
@@ -1,10 +1,10 @@
 'use strict'
 
-let imgNum = 18
-let gImgs = []
+var imgNum = 18
+var gImgs = []
 _createImgs(imgNum)
 
-let gSelectedImgIdx = 0
+var gSelectedImgIdx = 0
 
 var gMeme = {
     selectedImgId: 0,
@@ -19,7 +19,7 @@ var gMeme = {
     ]
 }
 
-var gKeywords = ['politics', 'funny', 'sarcastic', 'crazy', 'animals']
+var gKeywords = ['politics', 'funny', 'sarcastic', 'crazy', 'animal', 'cute', 'kids', 'movies']
 
 function getMeme() {
     return gMeme
@@ -27,6 +27,10 @@ function getMeme() {
 
 function getImg() {
     return gImgs[gSelectedImgIdx].url
+}
+
+function getKeywords() {
+    return gKeywords
 }
 
 function setLineTxt(text, idx) {
@@ -87,8 +91,12 @@ function changeToClickedLine(clickedLine) {
     gMeme.selectedLineIdx = clickedLine
 }
 
+function setFilterBy(elKeyword) {
+    return gImgs.filter(img => img.keywords.includes(elKeyword))
+}
+
 function loadGallery() {
-    _createImgs(imgNum)
+    // _createImgs(imgNum)
     return gImgs
 }
 
@@ -108,7 +116,6 @@ function getResetMemeLines() {
 function setLineDrag(drag) {
     gMeme.lines[gMeme.selectedLineIdx].isDrag = drag
 }
-
 
 function getEvPos(ev) {
     let pos = {
@@ -151,4 +158,26 @@ function _createImg(id, url, keywords) {
         keywords,
     }
 }
+
+gImgs = [
+    { id: 1, url: 'meme-imgs-square/1.jpg', keywords: ['politics', 'funny'] },
+    { id: 2, url: 'meme-imgs-square/2.jpg', keywords: ['animal', 'cute'] },
+    { id: 3, url: 'meme-imgs-square/3.jpg', keywords: ['animal', 'cute', 'kids'] },
+    { id: 4, url: 'meme-imgs-square/4.jpg', keywords: ['animal', 'cute'] },
+    { id: 5, url: 'meme-imgs-square/5.jpg', keywords: ['funny', 'cute', 'kids'] },
+    { id: 6, url: 'meme-imgs-square/6.jpg', keywords: ['funny', 'sarcastic'] },
+    { id: 7, url: 'meme-imgs-square/7.jpg', keywords: ['funny', 'cute', 'kids'] },
+    { id: 8, url: 'meme-imgs-square/8.jpg', keywords: ['funny', 'crazy'] },
+    { id: 9, url: 'meme-imgs-square/9.jpg', keywords: ['funny', 'kids'] },
+    { id: 10, url: 'meme-imgs-square/10.jpg', keywords: ['politics'] },
+    { id: 11, url: 'meme-imgs-square/11.jpg', keywords: ['funny'] },
+    { id: 12, url: 'meme-imgs-square/12.jpg', keywords: ['funny', 'sarcastic'] },
+    { id: 13, url: 'meme-imgs-square/13.jpg', keywords: ['funny', 'sarcastic'] },
+    { id: 14, url: 'meme-imgs-square/14.jpg', keywords: ['funny', 'sarcastic', 'movies'] },
+    { id: 15, url: 'meme-imgs-square/15.jpg', keywords: ['funny', 'sarcastic', 'movies'] },
+    { id: 16, url: 'meme-imgs-square/16.jpg', keywords: ['funny', 'movies'] },
+    { id: 17, url: 'meme-imgs-square/17.jpg', keywords: ['politics'] },
+    { id: 18, url: 'meme-imgs-square/18.jpg', keywords: ['funny', 'movies'] },
+]
+
 
